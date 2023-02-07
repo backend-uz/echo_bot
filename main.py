632a0f2717d98aedd2195c0c5914de001804c0d6
Jalilov <javohirjaliloff@gmail.com>
2023-02-07 20:31:53 +0500

    sendmessage, getupdates function create

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import requests
 
 TOKEN = '5661659754:AAGPPm7mkuylCre31dUmOBcXN3cgULdoqSQ'
-# url getUpdates
-url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
-response = requests.get(url)
-data = response.json()
-chat_id = data['result'][0]['message']['chat']['id']
 
-print(chat_id)
+def get_last_updates():
+    """
+    Use this function to get updates from Telegram.
+
+    Args:
+        None
+    Returns:
+        int(chat_id): Telegram chat id
+        str(text): Message text
+        int(update_id): Telegram update id
+    """
+    pass
+
+def send_message(chat_id, text):
+    """
+    Use this function to send text messages.
+
+    Args:
+        chat_id (int): Telegram chat id
+        text (str): Message text
+    Returns:
+        None
+    """
+    pass
